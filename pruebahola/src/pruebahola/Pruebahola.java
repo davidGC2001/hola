2c377d277870e7f59f88e774b18b981d827d7042
davidGC2001 <60681707+davidGC2001@users.noreply.github.com>
2020-04-07 03:41:39 -0500

    hola

---
+++ pruebahola/src/pruebahola/Pruebahola.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebahola;
+
+/**
+ *
+ * @author ACER TRITON 700
+ */
+public class Pruebahola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("hola");
+    }
+    
+}
